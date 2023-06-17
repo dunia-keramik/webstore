5175b0b4a6c781f8a1a01106179ef7839381f3a2
sarrahman <sarrahman.dev@gmail.com>
2023-06-18 02:25:40 +0800

    fix bug error data fetching

--- src/components/oraganisms/appBar/index.tsx
+++ src/components/oraganisms/appBar/index.tsx
@@ -1,27 +1,12 @@
-"use client";
 import { GetDataApi } from "@/src/utils";
-import { useEffect, useState } from "react";
 
-export default function AppBar() {
-  const [webstore, setwebstore] = useState({} as any);
+async function AppBar() {
+  const response = await GetDataApi(
+    `${process.env.NEXT_PUBLIC_HOST}/webstore/domain/ikjewffi.netlify.app`
+  );
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await GetDataApi(
-          `${process.env.NEXT_PUBLIC_HOST}/webstore/domain/ikjewffi.netlify.app`
-        );
-        if (response.status === 500) {
-          window.location.reload();
-        }
-        setwebstore(response.data);
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      }
-    };
+  const webstore = response.data;
 
-    fetchData();
-  }, []);
   return (
     <div className="flex justify-center items-center bg-slate-900 text-white p-3 md:p-5">
       <p className="font-bold text-lg md:text-xl">
@@ -30,3 +15,5 @@ export default function AppBar() {
     </div>
   );
 }
+
+export default AppBar;
--- src/components/oraganisms/catalogProducts/index.tsx
+++ src/components/oraganisms/catalogProducts/index.tsx
@@ -1,28 +1,10 @@
-"use client";
 import { GetDataApi } from "@/src/utils";
-import { useEffect, useState } from "react";
 import { CardProduct } from "../../molecules";
 
-const CatalogProducts = () => {
-  const [products, setProducts] = useState([] as any);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await GetDataApi(
-          `${process.env.NEXT_PUBLIC_HOST}/barang`
-        );
-        if (response.status === 500) {
-          window.location.reload();
-        }
-        setProducts(response.data);
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      }
-    };
+const CatalogProducts = async () => {
+  const response = await GetDataApi(`${process.env.NEXT_PUBLIC_HOST}/barang`);
 
-    fetchData();
-  }, []);
+  const products = response.data;
 
   return (
     <div className="flex">
