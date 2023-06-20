3b89d2db7013926c14e5131f359e96edca59d989
sarrahman <sarrahman.dev@gmail.com>
2023-06-20 17:24:44 +0800

    menambahkan revalidate data setiap 1 jam

--- src/utils/fetching/index.ts
+++ src/utils/fetching/index.ts
@@ -1,8 +1,8 @@
 export async function GetDataApi(url: string): Promise<any> {
   try {
     const response = await fetch(
-      url
-      // { next: { revalidate: 3600 } }
+      url,
+      { next: { revalidate: 3600 } }
     );
     const data = await response.json();
     return data;
