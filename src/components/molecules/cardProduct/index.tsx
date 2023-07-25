3ec3b895278b186d5ea6037930a063680dc5dc8b
sarrahman-me <sarrahman.me@gmail.com>
2023-07-25 19:49:48 +0800

    penanda stok terbatas

--- src/components/molecules/cardProduct/index.tsx
+++ src/components/molecules/cardProduct/index.tsx
@@ -6,7 +6,7 @@ const CardProduct = (props: { product: any }) => {
   const router = useRouter();
 
   // Cek apakah stok berada di bawah atau sama dengan 50
-  const isOutOfStock = props.product?.stok <= 0;
+  const isOutOfStock = props.product?.stok <= 100;
 
   // Format teks untuk kualitas (huruf pertama besar, sisanya kecil)
   const formattedKualitas =
@@ -18,9 +18,7 @@ const CardProduct = (props: { product: any }) => {
       onClick={() => {
         router.push(`/products/${props.product.slug}`);
       }}
-      className={`shadow m-2 cursor-pointer ${
-        isOutOfStock ? "opacity-50" : "" // Tambahkan opasitas jika stok habis
-      }`}
+      className={`shadow m-2 cursor-pointer`}
     >
       <div className="flex justify-center">
         <img
@@ -34,14 +32,16 @@ const CardProduct = (props: { product: any }) => {
       </p>
       <div
         className={`text-xs rounded-full py-1 px-2 mt-2 ${
-          isOutOfStock ? "bg-red-500 text-white" : ""
+          isOutOfStock ? "" : ""
         }`}
       >
         <div className="flex justify-between">
           <div>
             {formattedKualitas}{" "}
             {isOutOfStock ? (
-              <span className="text-sm font-semibold">(Stok Habis)</span>
+              <span className="text-sm font-semibold text-red-600">
+                ({props.product?.stok})
+              </span>
             ) : (
               <span className="text-sm font-semibold">
                 ({props.product?.stok})
