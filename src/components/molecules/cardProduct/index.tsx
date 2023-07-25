179c59822e7f14fc0d294212a9913b5697aaacaa
sarrahman-me <sarrahman.me@gmail.com>
2023-07-25 13:12:17 +0800

    :lipstick: merapikan product layouts

--- src/components/molecules/cardProduct/index.tsx
+++ src/components/molecules/cardProduct/index.tsx
@@ -10,13 +10,15 @@ const CardProduct = (props: { product: any }) => {
       onClick={() => {
         router.push(`/products/${props.product.slug}`);
       }}
-      className="border m-2 cursor-pointer"
+      className="shadow m-2 cursor-pointer"
     >
-      <img
-        className="w-full object-cover"
-        src={props.product?.images[0]}
-        alt={props.product?.nama_barang}
-      />
+      <div className="flex justify-center">
+        <img
+          className="md:w-44 md:h-44 w-32 h-32 object-cover"
+          src={props.product?.images[0]}
+          alt={props.product?.nama_barang}
+        />
+      </div>
       <p className="text-center font-bold">{props.product?.nama_barang}</p>
     </div>
   );
