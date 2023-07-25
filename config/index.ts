adc54e18f6b645992102abd8124fef38e980d9c1
sarrahman-me <sarrahman.me@gmail.com>
2023-07-25 16:13:04 +0800

    :lipstick: penyesuaian tampilan catalog

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST || "https://backend-dunia-keramik.vercel.app",
+  NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST || "http://localhost:5000",
 };
 
 export default config;
--- src/components/molecules/cardProduct/index.tsx
+++ src/components/molecules/cardProduct/index.tsx
@@ -5,12 +5,17 @@ import { useRouter } from "next/navigation";
 const CardProduct = (props: { product: any }) => {
   const router = useRouter();
 
+  // Cek apakah stok berada di bawah atau sama dengan 50
+  const isOutOfStock = props.product?.stok <= 0;
+
   return (
     <div
       onClick={() => {
         router.push(`/products/${props.product.slug}`);
       }}
-      className="shadow m-2 cursor-pointer"
+      className={`shadow m-2 cursor-pointer ${
+        isOutOfStock ? "opacity-50" : "" // Tambahkan opasitas jika stok habis
+      }`}
     >
       <div className="flex justify-center">
         <img
@@ -20,6 +25,18 @@ const CardProduct = (props: { product: any }) => {
         />
       </div>
       <p className="text-center font-bold">{props.product?.nama_barang}</p>
+      <div
+        className={`text-xs rounded-full py-1 px-2 mt-2 ${
+          isOutOfStock ? "bg-red-500 text-white" : ""
+        }`}
+      >
+        {props.product?.kualitas}{" "}
+        {isOutOfStock ? (
+          <span className="text-sm font-semibold">(Stok Habis)</span>
+        ) : (
+          <span className="text-sm font-semibold">({props.product?.stok})</span>
+        )}
+      </div>
     </div>
   );
 };
