680f685a0d54447a71b9822515dfdfcd1a821bd7
muhammad nur rahman <138513675+sarrahman-me@users.noreply.github.com>
2023-08-04 10:54:57 +0800

    Update index.tsx

--- src/components/molecules/textFooter/index.tsx
+++ src/components/molecules/textFooter/index.tsx
@@ -1,9 +1,9 @@
 export default function TextFooter() {
   return (
     <p className="text-sm font-light text-center text-gray-500 dark:text-gray-400 mt-10">
-      Powered By{" "}
+      Created By{" "}
       <a
-        href="https://github.com/sarrahman"
+        href="https://github.com/sarrahman-me"
         target="_blank"
         rel="noopener noreferrer"
         className="font-medium text-sky-600 hover:underline dark:text-sky-500"
