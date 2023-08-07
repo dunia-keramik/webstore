52c1cb8e4a80cb7554e0872feb2b317780a50632
muhammad nur rahman <138513675+sarrahman-me@users.noreply.github.com>
2023-08-07 08:01:51 +0800

    Update message whatsapp

--- src/components/molecules/whatsappButton/index.tsx
+++ src/components/molecules/whatsappButton/index.tsx
@@ -3,7 +3,13 @@ import { Button } from "../../atoms";
 
 export default function WhatsappButton(props: { barang: any; phone: string }) {
   const handleWhatsAppClick = () => {
-    const message = `Aku ingin bertanya tentang *${props.barang?.nama_barang}* merk *${props.barang?.merk}* dan ukuran *${props.barang?.ukuran}* dengan kualitas *${props.barang?.kualitas}* harga *${props.barang?.harga}* dan saat ini stok *${props.barang?.stok}*`;
+    const message = `Saya ingin bertanya tentang produk ini
+    Nama : *${props.barang?.nama_barang}*,
+    Merk : *${props.barang?.merk}*, 
+    Ukuran : *${props.barang?.ukuran}*, 
+    Kualitas : *${props.barang?.kualitas}*,
+    Harga : *${props.barang?.harga}* ,
+    Stok : *${props.barang?.stok}*`;
     const whatsappLink = `https://api.whatsapp.com/send?phone=${
       props.phone
     }&text=${encodeURIComponent(message)}`;
